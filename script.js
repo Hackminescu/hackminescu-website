398c49fdc8376cab20fa4946d567caf470489d95
Cosmin <cosmin.matei@eminescubucuresti.ro>
2022-11-06 20:50:17 +0200

    +timer

--- script.js
+++ script.js
@@ -16,6 +16,34 @@ const animatie = () =>{
         meniu.classList.toggle('toggle');
    });
 }
+var countDownDate = new Date("Jan 5, 2023 18:00:00").getTime();
+
+var x = setInterval(function() {
+
+
+  var now = new Date().getTime();
+
+
+  var distance = countDownDate - now;
+
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+  document.getElementById("timer").innerHTML = "Starts in: " + days + "D " + hours + "H "
+  + minutes + "M " + seconds + "S ";
+
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "Chiar s-a intamplat!";
+  }
+
+}, 1000);
+
     animatie();
 
 
