1ed7d4029fa60a23660a7e6bfc2dc144ba130183
Adi <adisorin.olteanu@yahoo.com>
2022-11-13 21:04:22 +0200

    Storing participants' registrations 🌌

--- homepage/script.js
+++ homepage/script.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         e.preventDefault();
         if (x < max_fields) {
             x++;
-           var html = '<div  class="align"><input type="text" placeholder="Participantul '+x+'" required> <img id="icon" class="delete" src="assets/minus.svg"></div>'
+           var html = '<div  class="align"><input type="text" placeholder="Participantul '+x+'" name="participant'+x+'" required> <img id="icon" class="delete" src="assets/minus.svg"></div>'
             $(wrapper).append(html); //add input box
         } else {
             alert('Maxim 4 participanti !')
@@ -21,31 +21,35 @@ $(document).ready(function() {
         x--;
     })
 });
+var reg_url = "hackminescu.com/register"
+
+// Registering participants and spectators
+var registrationForm = document.getElementById("partReg");
+console.log(document.forms)
+
+registrationForm.addEventListener("submit", e => {
+    e.preventDefault();
+    fetch(reg_url, {
+        method: "POST",
+        body: new FormData(registrationForm)
+    })
+    .then(response => console.log("Success!", response))
+    .catch(error => console.error('Error!', error.message))
+})
 
 var countDownDate = new Date("Nov 27, 2022 18:00:00").getTime();
 
 var x = setInterval(function() {
-
-
   var now = new Date().getTime();
-
-
   var distance = countDownDate - now;
-
-
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-
   document.getElementById("timer").innerHTML = "Evenimentul incepe in: <br> " + days + " Zile " + hours + " Ore "
   + minutes + " Minute " + seconds + " Secuunde ";
-
-
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("timer").innerHTML = "Chiar s-a intamplat!";
   }
-
 }, 1000);
--- main.go
+++ main.go
@@ -1,58 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-type Participant struct {
-	Name  string `json:"name"`
-	Class string `json:"class"`
-	Phone string `json:"phone_number"`
-	Email string `json:"email"`
-}
-
-type Registration struct {
-	Participants       []Participant `json:"participants"`
-	ProjectName        string        `json:"project_name"`
-	ProjectDescription string        `json:"project_description"`
-	SpectatorName      string        `json:"spectator_name"`
-}
-
-// Registration method for both participants and spectators
-func registration(w http.ResponseWriter, r *http.Request) {
-	// Only POST requests are accepted for registration
-	if r.Method != "POST" {
-		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-	}
-
-	// registration_type stores wether a participant or
-	// spectator has registered.
-	registration_type := r.Header.Get("registration_type")
-
-	b, err := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	// Reading the request as JSON and storing participants and
-	// the project's details.
-	registration := Registration{}
-	json.Unmarshal([]byte(b), &registration)
-	fmt.Println(registration_type, registration)
-}
-
 func main() {
 	port := "8000"
 	router := mux.NewRouter()
-
-	router.PathPrefix("/participant-registration").HandlerFunc(registration).Methods("POST")
 
 	router.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("./homepage"))))
 
